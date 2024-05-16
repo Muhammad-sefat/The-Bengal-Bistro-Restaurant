1601d6b25e486bd509570492a4328533cf68385e
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-05-16 17:11:13 +0600

    Create Dynamic Route in Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,47 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   const navMenu = (
     <>
       <li>
-        <a>Item 1</a>
+        <NavLink
+          to={"/"}
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <a>Home</a>
+        </NavLink>
       </li>
       <li>
-        <a>Parent</a>
+        <NavLink
+          to={"/contact-us"}
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <a>Contact Us</a>
+        </NavLink>
       </li>
       <li>
-        <a>Item 3</a>
+        <NavLink
+          to={"/dashboard"}
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <a>Dashboard</a>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/our-menu"}
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <a>Our Menu</a>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/our-shop"}
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          <a>Our Shop</a>
+        </NavLink>
       </li>
     </>
   );
@@ -45,7 +78,57 @@ const Navbar = () => {
           </a>
         </div>
         <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{navMenu}</ul>
+          <ul className="menu menu-horizontal px-1 font-semibold">{navMenu}</ul>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import router from "./Components/Router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="max-w-screen-lg mx-auto">
+    <div className="max-w-screen-xl mx-auto">
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>
